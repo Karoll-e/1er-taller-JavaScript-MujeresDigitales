f3039d4d3420578ebeddbc6aa3da191253b5673c
Karoll Escalante <karollescalante02@gmail.com>
2024-08-25 15:18:56 -0500

    initial commit

---
+++ 01-variables-condicionales.js
@@ -0,0 +1,11 @@
+/* Declara una variable llamada “temperatura”. Si la temperatura es mayor a 30 grados,
+muestra un mensaje que diga "Hace calor", de lo contrario, muestra "Hace frío".
+ */
+
+let temperatura = 20;
+
+if (temperatura > 30) {
+  console.log("Hace calor");
+} else {
+  console.log("Hace frio");
+}
